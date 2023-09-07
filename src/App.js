e93139b1a8e10e540f769bccbc978de8f85f3eb5
Akash <yogibabu32@gmail.com>
2023-09-06 21:24:31 -0700

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState();
+  const onchangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+  const onclickHandler = (e) => {
+    // e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+  console.log(todos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <div>
+            <h1>Todo App</h1>
+          </div>
+          <div style={{ display: "flex" }}>
+            <div>
+              <input
+                type="text"
+                placeholder="enter your todo"
+                value={input}
+                onChange={onchangeHandler}
+              />
+            </div>
+            <div>
+              <button style={{ marginLeft: "20px" }} onClick={onclickHandler}>
+                add todo
+              </button>
+            </div>
+          </div>
+          <div style={{}}>
+            <ul>
+              {todos.map((obj) => (
+                <li
+                  style={{
+                    width: "400px",
+                    backgroundColor: "blue",
+                    marginTop: "20px",
+                    padding: "10px",
+                  }}
+                >
+                  {obj}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
